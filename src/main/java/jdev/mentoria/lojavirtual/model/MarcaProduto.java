08f0b135af56828a74a8fd0d8b1580796e9e6b13
hailton costa <jhailtoncosta@gmail.com>
2022-10-13 02:29:52 -0300

    commit classe pessoa, pessoafisica, pessoajuridica

---
+++ src/main/java/jdev/mentoria/lojavirtual/model/MarcaProduto.java
@@ -0,0 +1,40 @@
+package jdev.mentoria.lojavirtual.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "marcaproduto")
+@SequenceGenerator(name = "seq_marcaproduto", sequenceName = "seq_marcaproduto", allocationSize = 1, initialValue = 1)
+public class MarcaProduto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marcaproduto")
+	private Long id;
+	
+	private String nomeDesc;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNomeDesc() {
+		return nomeDesc;
+	}
+
+	public void setNomeDesc(String nomeDesc) {
+		this.nomeDesc = nomeDesc;
+	}
+}
